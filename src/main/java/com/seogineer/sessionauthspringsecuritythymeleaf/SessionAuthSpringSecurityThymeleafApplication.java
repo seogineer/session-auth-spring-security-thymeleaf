5031470c3b8150111e39d930f22dd36c74fd6b7e
seogineer <dgseo8981@gmail.com>
2023-03-04 20:15:14 +0900

    fix: 프로젝트 이름 오타 수정

--- src/main/java/com/seogineer/sessionauthspringsecuritythymeleaf/SessionAuthSpringSecurityThymeleafApplication.java
+++ src/main/java/com/seogineer/sessionauthspringsecuritythymeleaf/SessionAuthSpringSecurityThymeleafApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SessionAuthSpringSecurityThymleafApplication {
+public class SessionAuthSpringSecurityThymeleafApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SessionAuthSpringSecurityThymleafApplication.class, args);
+        SpringApplication.run(SessionAuthSpringSecurityThymeleafApplication.class, args);
     }
 
 }
